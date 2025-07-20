6e4afae5bd0b2ecffdbe12cad296f00dd86d7b5a
Tesfalem <tesfalemabera2@gmail.com>
2025-07-20 21:07:13 +0300

    Update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,7 @@
-import UserProfile from './components/UserProfile';
+
+import Header from './components/Header';
+import MainContent from '.components/MainContent';
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -8,6 +11,7 @@ function App() {
         age={25} 
         bio="Loves hiking and photography" 
       />
+      
     </div>
   );
 }
