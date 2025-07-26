4a13a200adcb6ab2a0a691c52619fdb14b02f210
Tesfalem <tesfalemabera2@gmail.com>
2025-07-26 16:50:42 +0300

    add this one

--- my-company/About.jsx
+++ my-company/About.jsx
@@ -14,31 +14,6 @@ function About() {
         we strive to deliver innovative solutions to our clients.
       </p>
     </div>
-  import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Navbar';
-import Home from './Home';
-import About from './About';
-import Services from './Services';
-import Contact from './Contact';
-import Footer from './Footer'; // Optional
-import './App.css';
-
-function App() {
-  return (
-    <Router>
-      <div style={{ minHeight: '100vh', position: 'relative' }}>
-        <Navbar />
-        <div style={{ paddingBottom: '60px' }}> {/* Adjust if using footer */}
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
-        <Footer /> {/* Optional */}
-      </div>
-    </Router>
   );
 }
 
