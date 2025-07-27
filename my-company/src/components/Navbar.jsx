a8fb926e4ecc8902f60fd7b23c7450ae0d19084c
Tesfalem <tesfalemabera2@gmail.com>
2025-07-27 17:02:59 +0300

    Add modifye

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav style={{ background: '#333', padding: '10px' }}>
-      <Link to="/" style={{ color: 'white', marginRight: '15px', textDecoration: 'none' }}>Home</Link>
-      <Link to="/about" style={{ color: 'white', marginRight: '15px', textDecoration: 'none' }}>About</Link>
-      <Link to="/services" style={{ color: 'white', marginRight: '15px', textDecoration: 'none' }}>Services</Link>
-      <Link to="/contact" style={{ color: 'white', textDecoration: 'none' }}>Contact</Link>
+      <Link to="/" style={{ color: '#fff', marginRight: '15px', textDecoration: 'none' }}>Home</Link>
+      <Link to="/about" style={{ color: '#fff', marginRight: '15px', textDecoration: 'none' }}>About</Link>
+      <Link to="/services" style={{ color: '#fff', marginRight: '15px', textDecoration: 'none' }}>Services</Link>
+      <Link to="/contact" style={{ color: '#fff', textDecoration: 'none' }}>Contact</Link>
     </nav>
   );
 }
