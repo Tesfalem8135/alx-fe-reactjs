b9c613300e0a8dc28109b6eba7b464ba4822c38e
Tesfalem <tesfalemabera2@gmail.com>
2025-07-27 16:42:56 +0300

    Add modifye

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,11 +1,8 @@
 
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Services from './components/Services';
-import Contact from './components/Contact';
+
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
 
 function RecipeSharing() {
   return (
