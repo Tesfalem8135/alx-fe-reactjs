680e71a7147619b98f3d08f134e65527c4acb785
Tesfalem <tesfalemabera2@gmail.com>
2025-07-27 16:38:24 +0300

    Add modifye

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,15 +1,36 @@
 
-import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
-import './App.css';
 
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+
+function RecipeSharing() {
   return (
     <div>
       <h1>Recipe Sharing App</h1>
       <AddRecipeForm />
       <RecipeList />
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/recipes" element={<RecipeSharing />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
