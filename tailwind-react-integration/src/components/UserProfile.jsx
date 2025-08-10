9d8a918bded1a29ac01d702c253942d39d7690f8
Tesfalem <tesfalemabera2@gmail.com>
2025-08-10 14:14:39 +0300

    update search

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -21,7 +21,7 @@ function UserProfile() {
         alt="User" 
         className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto"
       />
-      <h1 className="text-lg md:text-xl text-blue-800 my-3 md:my-4">
+      <h1 className="text-lg md:text-xl text-blue-800 my-4">
         John Doe
       </h1>
       <p className="text-sm md:text-base text-gray-600">
